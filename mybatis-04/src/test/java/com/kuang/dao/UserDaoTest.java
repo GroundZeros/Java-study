9b0cf2d20c24d75b1a9b9f5b5af3291204cc771d
hiki <742565643@qq.com>
2021-04-23 14:06:46 +0800

    回滚测试

--- mybatis-04/src/test/java/com/kuang/dao/UserDaoTest.java
+++ mybatis-04/src/test/java/com/kuang/dao/UserDaoTest.java
@@ -37,7 +37,7 @@ public class UserDaoTest {
     @Test
     public void testLog4j() {
         logger.info("info:进入了testLog4j");
-        logger.debug("debug:进入了testLog4j");
+        logger.error("error:进入了testLog4j");
 
     }
 
